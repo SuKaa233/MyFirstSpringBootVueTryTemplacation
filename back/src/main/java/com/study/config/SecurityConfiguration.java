fb523934094ebc4adfb9a5d454801a91d2fecce4
suka <2579362548@qq.com>
2026-01-10 00:17:19 +0800

    记住我的功能的实现这个springsecurity的新版本的持久化set数据源的方法弃用了新的但是还没实现所以只能用idea禁用语句检查那一行了

--- back/src/main/java/com/study/config/SecurityConfiguration.java
+++ back/src/main/java/com/study/config/SecurityConfiguration.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,10 +15,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 
 
@@ -29,6 +31,11 @@ public class SecurityConfiguration {
 
     @Resource
     AuthorizeService authorizeService;
+
+    @Resource
+    DataSource dataSource;
+
+    public
 
     @Bean
     SecurityFilterChain chain(HttpSecurity http) {
@@ -44,13 +51,24 @@ public class SecurityConfiguration {
                         .logoutSuccessHandler(this::onAuthenticationSuccess)
                         .logoutUrl("/api/auth/logout")
                 )
-                .exceptionHandling(e->e
+                .exceptionHandling(e -> e
                         .authenticationEntryPoint(this::AuthenticationFailureHandler)
                 )
+                .rememberMe(rm -> rm.rememberMeParameter("remember").tokenRepository(tokenRepository(dataSource)).tokenValiditySeconds(3600*24*7))
                 .cors(c -> c.configurationSource(corsConfigurationSource()))
                 .userDetailsService(authorizeService)
                 .build();
     }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository(DataSource dataSource) {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        //noinspection removal
+        tokenRepository.setDataSource(dataSource);
+        tokenRepository.setCreateTableOnStartup(true);
+        return tokenRepository;
+    }
+
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
@@ -58,16 +76,18 @@ public class SecurityConfiguration {
         cfg.addAllowedOrigin("http://localhost:5173"); // 前端端口
         cfg.addAllowedHeader("*");
         cfg.addAllowedMethod("*");
-        cfg.setAllowCredentials(true);
+        cfg.setAllowCredentials(false);//第一次运行记得打开 留给以后的自己
         UrlBasedCorsConfigurationSource src = new UrlBasedCorsConfigurationSource();
         src.registerCorsConfiguration("/**", cfg);
         return src;
     }
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
 //    @Bean
 //    public AuthenticationManager authenticationManager(HttpSecurity security){
 //        return security
@@ -80,17 +100,16 @@ public class SecurityConfiguration {
     private void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException {
         httpServletRequest.setCharacterEncoding("utf-8");
         httpServletResponse.setContentType("application/json;charset=UTF-8");
-        if(httpServletRequest.getRequestURI().endsWith("/login")){
+        if (httpServletRequest.getRequestURI().endsWith("/login")) {
             httpServletResponse.getWriter().write(JSONObject.toJSONString(RestBean.success("登录成功！")));
-        }
-        else if(httpServletRequest.getRequestURI().endsWith("/logout")){
+        } else if (httpServletRequest.getRequestURI().endsWith("/logout")) {
             httpServletResponse.getWriter().write(JSONObject.toJSONString(RestBean.success("退出登录成功！")));
         }
     }
 
     public void AuthenticationFailureHandler(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
         response.setCharacterEncoding("utf-8");
-        response.getWriter().write(JSONObject.toJSONString(RestBean.failure(401,exception.getMessage())));
+        response.getWriter().write(JSONObject.toJSONString(RestBean.failure(401, exception.getMessage())));
     }
 
 }
